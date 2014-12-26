5268a81cdf065cc6b431ecc5637fbba73841309c
badlogic <badlogicgames@gmail.com>
2014-12-26 09:36:23 +0100

    first code commit

---
+++ src/main/java/com/badlogicgames/quack/Quack.java
@@ -0,0 +1,7 @@
+package com.badlogicgames.quack;
+
+public class Quack {
+	public static void main (String[] args) {
+		System.out.println("Quack");
+	}
+}
---
+++ src/test/java/com/badlogicgames/quack/QuackTest.java
@@ -0,0 +1,12 @@
+package com.badlogicgames.quack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuackTest {
+	@Test
+	public void testQuack() {
+		assertTrue(true);
+	}
+}
